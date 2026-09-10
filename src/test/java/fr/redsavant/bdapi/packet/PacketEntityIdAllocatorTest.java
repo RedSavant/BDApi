08252a75fea81b240bea7a458ba9bcf57c8dcf01
el211 <elias.saire@ynov.com>
2026-09-10 10:57:18 +0200

    test: cover anchor offset semantics and id allocator

---
+++ src/test/java/fr/redsavant/bdapi/packet/PacketEntityIdAllocatorTest.java
@@ -0,0 +1,29 @@
+package fr.redsavant.bdapi.packet;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class PacketEntityIdAllocatorTest {
+
+    @Test
+    void producesUniqueIds() {
+        PacketEntityIdAllocator allocator = new PacketEntityIdAllocator(0);
+        Set<Integer> seen = new HashSet<>();
+        for (int i = 0; i < 1000; i++) {
+            assertTrue(seen.add(allocator.next()));
+        }
+        assertEquals(1000, seen.size());
+    }
+
+    @Test
+    void incrementsMonotonically() {
+        PacketEntityIdAllocator allocator = new PacketEntityIdAllocator(10);
+        assertEquals(11, allocator.next());
+        assertEquals(12, allocator.next());
+    }
+}
