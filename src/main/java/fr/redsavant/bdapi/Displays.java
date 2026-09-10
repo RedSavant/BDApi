7fdc0f0a7e3ee292b747a4b3a893b2ffdd844e69
el211 <elias.saire@ynov.com>
2026-09-10 10:49:45 +0200

    feat: add backend selection and viewers to builder

--- src/main/java/fr/redsavant/bdapi/Displays.java
+++ src/main/java/fr/redsavant/bdapi/Displays.java
@@ -1,15 +1,14 @@
 package fr.redsavant.bdapi;
 
 import fr.redsavant.bdapi.builder.BlockDisplayBuilder;
+import fr.redsavant.bdapi.display.DisplayBackend;
 import fr.redsavant.bdapi.group.DisplayGroup;
 import fr.redsavant.bdapi.internal.Animator;
 import fr.redsavant.bdapi.internal.DisplayRegistry;
 import fr.redsavant.bdapi.internal.PhysicsEngine;
+import fr.redsavant.bdapi.packet.PacketDisplaySender;
+import fr.redsavant.bdapi.packet.PacketEntityIdAllocator;
 import org.bukkit.plugin.Plugin;
-
-/**
- * Endpoint to create displays
- */
 
 public final class Displays {
 
@@ -17,12 +16,20 @@ public final class Displays {
     private final DisplayRegistry registry;
     private final Animator animator;
     private final PhysicsEngine physicsEngine;
+    private final DisplayBackend defaultBackend;
+    private final PacketDisplaySender packetSender;
+    private final PacketEntityIdAllocator entityIdAllocator;
 
-    public Displays(Plugin plugin, DisplayRegistry registry, Animator animator, PhysicsEngine physicsEngine) {
+    public Displays(Plugin plugin, DisplayRegistry registry, Animator animator, PhysicsEngine physicsEngine,
+                    DisplayBackend defaultBackend, PacketDisplaySender packetSender,
+                    PacketEntityIdAllocator entityIdAllocator) {
         this.plugin = plugin;
         this.registry = registry;
         this.animator = animator;
         this.physicsEngine = physicsEngine;
+        this.defaultBackend = defaultBackend;
+        this.packetSender = packetSender;
+        this.entityIdAllocator = entityIdAllocator;
     }
 
     public BlockDisplayBuilder create() {
@@ -36,8 +43,24 @@ public final class Displays {
     public Plugin plugin() {
         return plugin;
     }
+
     public DisplayRegistry registry() {
         return registry;
     }
-    public Animator animator() { return animator; }
+
+    public Animator animator() {
+        return animator;
+    }
+
+    public DisplayBackend defaultBackend() {
+        return defaultBackend;
+    }
+
+    public PacketDisplaySender packetSender() {
+        return packetSender;
+    }
+
+    public PacketEntityIdAllocator entityIdAllocator() {
+        return entityIdAllocator;
+    }
 }
